29c4e83d4353d25f06d552dcf6d4e0a163da63c2
ybressler <yaakov.bressler@ginger.io>
2024-05-28 16:44:04 -0700

    Update readme.

--- src/create_data/main.py
+++ src/create_data/main.py
@@ -15,7 +15,7 @@ DATA_SOURCE_S3_BUCKET = "yb-big-data-workshop-1"
 
 
 if __name__ == "__main__":
-    s3_client = boto3.Session().client("s3")
+    s3_client = boto3.Session(profile_name="yb-personal").client("s3")
     s3_service = ServiceBase(s3_client=s3_client, bucket_name=DATA_SOURCE_S3_BUCKET)
 
     file_name = Path("tmp/measurements.txt.gz")
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     file_name.parent.mkdir(parents=True, exist_ok=True)
 
     measurement = CreateMeasurement()
-    measurement.generate_measurement_file(file_name=file_name.as_posix(), records=1_000_000_000, compressed=True)
+    measurement.generate_measurement_file(file_name=file_name.as_posix(), records=10_000_000, compressed=False)
     print("finished generating the data")
 
     dt_start = datetime.datetime.now()
--- src/process_data/main.py
+++ src/process_data/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Process a measurements file:
     1. Get data from URL
@@ -9,6 +10,7 @@ Process a measurements file:
 
 Code credits: https://github.com/ifnesi/1brc#submitting
 """
+
 import argparse
 import shutil
 
@@ -21,36 +23,34 @@ def do_thing(file_name: str):
 
     new_file_name = file_name.replace(".gz", "")
 
-    with pgzip.open(file_name, 'rb') as f_in, open(file_name.replace(".gz", ""), 'wb') as f_out:
+    with pgzip.open(file_name, "rb") as f_in, open(file_name.replace(".gz", ""), "wb") as f_out:
         shutil.copyfileobj(f_in, f_out)
 
     # Can't open in context, otherwise, will need to load all data in memory...
     df = (
-        pl.scan_csv(new_file_name, separator=";", has_header=False, with_column_names=lambda cols: ["station_name", "measurement"])
+        pl.scan_csv(
+            new_file_name,
+            separator=";",
+            has_header=False,
+            with_column_names=lambda cols: ["station_name", "measurement"],
+        )
         .group_by("station_name")
         .agg(
             pl.min("measurement").alias("min_measurement"),
             pl.mean("measurement").alias("mean_measurement"),
-            pl.max("measurement").alias("max_measurement")
+            pl.max("measurement").alias("max_measurement"),
         )
         .sort("station_name")
         .collect(streaming=True)
     )
 
-    df.write_csv(new_file_name.replace(".txt","") + " - results.csv", separator=",")
+    df.write_csv(new_file_name.replace(".txt", "") + " - results.csv", separator=",")
     return df
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Analyze measurement file")
-    parser.add_argument(
-        "-f",
-        "--file_name",
-        dest="file_name",
-        type=str,
-        help='File name'
-    )
+    parser.add_argument("-f", "--file_name", dest="file_name", type=str, help="File name")
     args = parser.parse_args()
 
     df = do_thing(args.file_name)
