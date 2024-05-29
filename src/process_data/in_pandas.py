6c8c35d3e5db8ef747e18b703a8ecb55af389ecd
ybressler <yaakov.bressler@ginger.io>
2024-05-28 17:13:40 -0700

    Do it in pandas

--- src/process_data/in_pandas.py
+++ src/process_data/in_pandas.py
@@ -3,10 +3,14 @@
 Process the stuff in pandas
 """
 
+import pandas as pd
 import argparse
 
 
-def do_thing(filename: str): ...
+def do_thing(filename: str):
+    df = pd.read_csv(filename, sep=";", header=None, names=["station_name", "measurement"])
+
+    print(df.head(2))
 
 
 if __name__ == "__main__":
